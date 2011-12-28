eb27bd1a1c66b93f72e11369cbb0939270de1b42
David Keegan <git@davidkeegan.com>
2011-12-28 11:27:27 -0800

    Adding a url handler callback method

---
+++ TwitterMacExample/TwitterMacExample/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  TwitterMacExample
+//
+//  Created by David Keegan on 12/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
