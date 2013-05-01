9addbdb7b53696f9c9e44bfe8f1ac598b593c1b4
Falko Buttler <buttler@me.com>
2013-05-01 00:09:10 -0700

    Added error handling when return query string doesn't contain oauth parameters This lead to a crash/assert before. This situation might occur if you quickly register/unregister with a service

--- AFOAuth1Client/AFOAuth1Client.m
+++ AFOAuth1Client/AFOAuth1Client.m
@@ -263,10 +263,18 @@ static inline NSString * AFHMACSHA1Signature(NSURLRequest *request, NSString *co
             currentRequestToken.verifier = [AFParametersFromQueryString([url query]) valueForKey:@"oauth_verifier"];
 
             [self acquireOAuthAccessTokenWithPath:accessTokenPath requestToken:currentRequestToken accessMethod:accessMethod success:^(AFOAuth1Token * accessToken) {
-                self.accessToken = accessToken;
-
-                if (success) {
-                    success(accessToken);
+                
+                if ( accessToken ) {
+                    self.accessToken = accessToken;
+                    
+                    if (success) {
+                        success(accessToken);
+                    }
+                }
+                else {
+                    if (failure) {
+                        failure(nil);
+                    }
                 }
             } failure:^(NSError *error) {
                 if (failure) {
@@ -395,6 +403,10 @@ static inline NSString * AFHMACSHA1Signature(NSURLRequest *request, NSString *co
     }
 
     NSDictionary *attributes = AFParametersFromQueryString(queryString);
+    
+    if ( attributes.count == 0 ) {
+        return nil;
+    }
 
     NSDate *expiration = nil;
     if (attributes[@"oauth_token_duration"]) {
