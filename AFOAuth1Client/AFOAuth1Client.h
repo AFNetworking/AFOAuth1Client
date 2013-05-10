438b23d77a47668c7596855cea808824ab39a013
Greg Spiers <greg.spiers@shinydevelopment.com>
2013-05-10 12:53:11 +0100

    Add a method to create a URLRequest that will not include the authentication header and will leave any oauth_ parameters intact. Fixes issue with Flickr where initial access request (created outside of the OAuth client with requestWithMethod) would have the oauth_token param removed and cause Flickr to show error message.

--- AFOAuth1Client/AFOAuth1Client.h
+++ AFOAuth1Client/AFOAuth1Client.h
@@ -103,6 +103,18 @@ typedef enum {
                                 success:(void (^)(AFOAuth1Token *accessToken, id responseObject))success
                                 failure:(void (^)(NSError *error))failure;
 
+///---------------------
+/// @name Requests
+///---------------------
+
+/**
+
+ */
+- (NSMutableURLRequest *)unauthenticatedRequestWithMethod:(NSString *)method
+                                      path:(NSString *)path
+                                parameters:(NSDictionary *)parameters;
+
+
 @end
 
 ///----------------
