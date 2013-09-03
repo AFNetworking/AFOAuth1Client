6c189cfd50f273c5e593cf40b251151251b360a5
Marc Nijdam <mnijdam@yahoo-inc.com>
2013-09-03 09:10:49 -0700

    Fixes #79. Don't ignore parameters on GET requests.

--- AFOAuth1Client/AFOAuth1Client.m
+++ AFOAuth1Client/AFOAuth1Client.m
@@ -398,7 +398,11 @@ static NSDictionary * AFKeychainQueryDictionaryWithIdentifier(NSString *identifi
 
     // Only use parameters in the HTTP POST request body (with a content-type of `application/x-www-form-urlencoded`).
     // See RFC 5849, Section 3.4.1.3.1 http://tools.ietf.org/html/rfc5849#section-3.4
-    NSDictionary *authorizationParameters = ([[request valueForHTTPHeaderField:@"Content-Type"] hasPrefix:@"application/x-www-form-urlencoded"] ? parameters : nil);
+    NSDictionary *authorizationParameters = parameters;
+    if ([method isEqualToString:@"POST"]) {
+        authorizationParameters = ([[request valueForHTTPHeaderField:@"Content-Type"] hasPrefix:@"application/x-www-form-urlencoded"] ? parameters : nil);
+    }
+    
     [request setValue:[self authorizationHeaderForMethod:method path:path parameters:authorizationParameters] forHTTPHeaderField:@"Authorization"];
     [request setHTTPShouldHandleCookies:NO];
     
