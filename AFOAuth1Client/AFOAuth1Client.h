a3272f725967a3df2c8d8434e6bd4cc3dfe92710
Mattt Thompson <m@mattt.me>
2014-11-23 21:48:38 -0800

    Adding documentation

--- AFOAuth1Client/AFOAuth1Client.h
+++ AFOAuth1Client/AFOAuth1Client.h
@@ -30,7 +30,9 @@ typedef NS_ENUM(NSUInteger, AFOAuthSignatureMethod) {
 @class AFOAuth1Token;
 
 /**
+ `AFOAuth1Client` encapsulates common patterns to authenticate against a resource server conforming to the behavior outlined in the OAuth 1.0a specification.
 
+ @see RFC 5849 The OAuth 1.0 Protocol: https://tools.ietf.org/html/rfc5849
  */
 @interface AFOAuth1Client : AFHTTPClient <NSCoding, NSCopying>
 
@@ -39,17 +41,17 @@ typedef NS_ENUM(NSUInteger, AFOAuthSignatureMethod) {
 ///-----------------------------------
 
 /**
-
+ The method used to create an OAuth signature. `AFPlainTextSignatureMethod` by default.
  */
 @property (nonatomic, assign) AFOAuthSignatureMethod signatureMethod;
 
 /**
-
+ The authentication realm.
  */
 @property (nonatomic, copy) NSString *realm;
 
 /**
-
+ The client's access token.
  */
 @property (nonatomic, strong) AFOAuth1Token *accessToken;
 
@@ -58,7 +60,11 @@ typedef NS_ENUM(NSUInteger, AFOAuthSignatureMethod) {
 ///---------------------
 
 /**
+ Initializes an `AFOAuth1Client` object with the specified base URL, key, and secret.
 
+ @param url The base URL for the HTTP client. This argument must not be `nil`.
+ @param key The client key.
+ @param secret The client secret.
  */
 - (id)initWithBaseURL:(NSURL *)url
                   key:(NSString *)key
@@ -69,7 +75,18 @@ typedef NS_ENUM(NSUInteger, AFOAuthSignatureMethod) {
 ///---------------------
 
 /**
-
+ Creates and enqueues `AFHTTPRequestOperation` objects to make the necessary request token and access token requests using the specified parameters. 
+ 
+ @discussion This is a convenience for doing `acquireOAuthRequestTokenWithPath:callbackURL:accessMethod:scope:success:failure` and then `acquireOAuthAccessTokenWithPath:requestToken:accessMethod:success:failure:`.
+ 
+ @param requestTokenPath The request token URL path.
+ @param userAuthorizationPath The user authorization URL path.
+ @param callbackURL The callback URL.
+ @param accessTokenPath The path for requesting an access token.
+ @param accessMethod The HTTP method for requesting an access token.
+ @param scope The requested authorization scope.
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the OAuth credential returned by the server, and the response object sent from the server.
+ @param failure A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes a single argument: the error returned from the server.
  */
 - (void)authorizeUsingOAuthWithRequestTokenPath:(NSString *)requestTokenPath
                           userAuthorizationPath:(NSString *)userAuthorizationPath
@@ -81,12 +98,14 @@ typedef NS_ENUM(NSUInteger, AFOAuthSignatureMethod) {
                                         failure:(void (^)(NSError *error))failure;
 
 /**
- @param path 
+ Creates and enqueues an `AFHTTPRequestOperation` to acquire an OAuth request token with the specified path, callback URL, access method, and scope.
+
+ @param path The URL path.
  @param callbackURL The URL to be set for `oauth_callback`. If `nil`, "oob" (out-of-band) is specified.
- @param accessMethod
- @param scope
- @param success
- @param failure
+ @param accessMethod The HTTP method.
+ @param scope The requested authorization scope.
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the OAuth credential returned by the server, and the response object sent from the server.
+ @param failure A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes a single argument: the error returned from the server.
  */
 - (void)acquireOAuthRequestTokenWithPath:(NSString *)path
                              callbackURL:(NSURL *)callbackURL
@@ -96,7 +115,13 @@ typedef NS_ENUM(NSUInteger, AFOAuthSignatureMethod) {
                                  failure:(void (^)(NSError *error))failure;
 
 /**
+ Creates and enqueues an `AFHTTPRequestOperation` to acquire an OAuth request token with the specified path, callback URL, access method, and scope.
 
+ @param path The URL method.
+ @param requestToken The request token.
+ @param accessMethod The HTTP method.
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the OAuth credential returned by the server, and the response object sent from the server.
+ @param failure A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes a single argument: the error returned from the server.
  */
 - (void)acquireOAuthAccessTokenWithPath:(NSString *)path
                            requestToken:(AFOAuth1Token *)requestToken
@@ -109,7 +134,10 @@ typedef NS_ENUM(NSUInteger, AFOAuthSignatureMethod) {
 ///----------------------------------------------------
 
 /**
- 
+ Sets the service provider request handler and completion handler blocks.
+
+ @param block A block to be executed when the request is made to the service provider. This block has no return value and takes a single argument: the request made to the service provider.
+ @param completion A block to be executed when the request is finished. This block has no return value and takes no arguments.
  */
 - (void)setServiceProviderRequestHandler:(void (^)(NSURLRequest *request))block
                               completion:(void (^)())completion;
@@ -133,52 +161,62 @@ extern NSString * const kAFApplicationLaunchOptionsURLKey;
 #pragma mark -
 
 /**
+ `AFOAuth1Token` models the credentials returned from an OAuth server, storing the key, secret, session, verifier, and whether the token is expired and can be renewed.
 
+ OAuth credentials can be stored in the user's keychain, and retrieved on subsequent launches.
  */
 @interface AFOAuth1Token : NSObject <NSCoding, NSCopying>
 
 /**
-
+ The OAuth token key.
  */
 @property (readonly, nonatomic, copy) NSString *key;
 
 /**
-
+ The OAuth token secret.
  */
 @property (readonly, nonatomic, copy) NSString *secret;
 
 /**
-
+ The OAuth token session.
  */
 @property (readonly, nonatomic, copy) NSString *session;
 
 /**
-
+ The OAuth token verifier.
  */
 @property (nonatomic, copy) NSString *verifier;
 
 /**
-
+ Whether the OAuth token can be renewed.
  */
 @property (readonly, nonatomic, assign, getter = canBeRenewed) BOOL renewable;
 
 /**
-
+ Whether the OAuth token is expired.
  */
 @property (readonly, nonatomic, assign, getter = isExpired) BOOL expired;
 
 /**
- 
+ Any additional information associated with the OAuth token.
  */
 @property (nonatomic, strong) NSDictionary *userInfo;
 
 /**
-
+ Initialize an OAuth token from a URL query string.
+ 
+ @param queryString The query of a URL containing the OAuth token credentials.
  */
 - (id)initWithQueryString:(NSString *)queryString;
 
 /**
-
+ Initializes an OAuth token with the specified key, secret, session, and expiration date.
+ 
+ @param key The OAuth token key.
+ @param secret The OAuth token secret.
+ @param session The OAuth token session.
+ @param expiration The OAuth token expiration date
+ @param renewable Whether the OAuth token can be renewed.
  */
 - (id)initWithKey:(NSString *)key
            secret:(NSString *)secret
@@ -234,3 +272,4 @@ extern NSString * const kAFApplicationLaunchOptionsURLKey;
 + (BOOL)deleteCredentialWithIdentifier:(NSString *)identifier;
 
 @end
+
